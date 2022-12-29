d38ab2d6ee6869a0652847fdf708b29cdff45cc4
prathameshshewale22 <prathameshs13@gmail.com>
2022-12-29 18:00:24 +0530

    ComplexNumber test oprator overloading

--- day1practice/ComplexNumber/Complex.cs
+++ day1practice/ComplexNumber/Complex.cs
@@ -1,4 +1,4 @@
-using Maths;
+namespace Maths;
 
 public class Complex{
     private int real;
@@ -7,6 +7,11 @@ public class Complex{
     public Complex(int r,int i){
         this.real=r;
         this.imag=i;
+    }
+
+    public Complex(){
+        this.real=0;
+        this.imag=0;
     }
    
    public int Real{
@@ -19,6 +24,13 @@ public class Complex{
     set{this.imag=value;}
    }
 
+
+   public static Complex operator+ (Complex c1,Complex c2){
+    Complex temp=new Complex();
+    temp.real=c1.real+c2.real;
+    temp.imag=c1.imag+c2.imag;
+    return temp;
+   }
    
 
 }
--- day1practice/ComplexNumber/Program.cs
+++ day1practice/ComplexNumber/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Maths;
+
+Complex c1=new Complex(10,20);
+
+Complex c2=new Complex(30,40);
+Complex c3=c1+c2;
+
+Console.WriteLine(c3.Real+" "+c3.Imag);
+
