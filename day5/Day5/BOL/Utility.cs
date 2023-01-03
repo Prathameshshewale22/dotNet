49db7e6c7a8007cdfb714fd594fbba87abb4b525
prathameshshewale22 <prathameshs13@gmail.com>
2023-01-04 00:37:18 +0530

    changes

---
+++ day5/Day5/BOL/Utility.cs
@@ -0,0 +1,55 @@
+namespace BOL;
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+public static class Utility{
+
+
+    public static Student GetById(int id,List<Student> stud){
+        foreach(Student st in stud){
+            if(st.Sid==id){
+                return st;
+            }
+        }
+        return null;
+    }
+
+    public static bool deleteReacord(int id,List<Student> stud){
+        foreach(Student st in stud){
+            if(st.Sid==id){
+                stud.Remove(st);
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+    public static void StoredData(List<Student>st){
+        try{
+
+            var options=new JsonSerializerOptions {IncludeFields=true};
+            var jsondata=JsonSerializer.Serialize<List<Student>>(st,options);
+            string fileName=@"E:\cdac\pg-dac\dotNet\localRepo_dotnet\dotnet\dotNet\day5\Day5\studnet.json";
+            File.WriteAllText(fileName,jsondata);
+            Console.WriteLine("data added in file");
+
+        }
+        catch(Exception e){
+
+        }
+        finally{
+
+        }
+    }
+
+    public static List<Student> GetDataFromFile(){
+        var options=new JsonSerializerOptions {IncludeFields=true};
+         string fileName=@"E:\cdac\pg-dac\dotNet\localRepo_dotnet\dotnet\dotNet\day5\Day5\studnet.json";
+         string jsondata= File.ReadAllText(fileName);
+         List<Student> st=JsonSerializer.Deserialize<List<Student>>(jsondata);
+         return st;
+        
+    }
+}
